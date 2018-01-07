dc9544b63646d71fee80f6b6eba0600b6b3f9e9b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-07 22:15:29 +0900

    Introduce prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: [
-    'xo-space/esnext',
+    'eslint-config-xo-space/esnext',
+    'eslint-config-prettier',
+    'eslint-config-prettier/standard',
   ],
   env: {
     node: true,
