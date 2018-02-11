bfcf1daa06bdf8bd956efe06541cb4c1ab31a67a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-02-11 23:13:08 +0900

    Use async/await for request handlers

--- src/handlers/request-handlers.js
+++ src/handlers/request-handlers.js
@@ -18,64 +18,62 @@ function _transformRequest(request) {
   };
 }
 
-function indexRequestHandler(req, res) {
+async function indexRequestHandler(req, res) {
   const status = req.query.status;
 
-  Request.findAll({
+  const requests = await Request.findAll({
     where: {
       memberId: req.user.id,
       status,
     },
     order: [['createdAt', 'ASC']],
-  }).then(requests => {
-    Promise.all([
-      User.findAll({
-        where: { id: requests.map(request => request.userId) },
-      }),
-      Label.findAll({
-        where: { id: requests.map(request => request.labelId) },
-      }),
-    ]).then(values => {
-      const users = values[0];
-      const labels = values[1];
-
-      const requests_ = requests.map(request => {
-        const request_ = {
-          id: request.id,
-          status: request.status,
-          createdAt: request.createdAt,
-          updatedAt: request.updatedAt,
-        };
-        users.forEach(user => {
-          if (request.userId === user.id) {
-            request_.member = {
-              id: user.id,
-              name: user.name,
-              imageUrl: user.imageUrl,
-            };
-          }
-        });
-        labels.forEach(label => {
-          if (request.labelId === label.id) {
-            request_.label = {
-              id: label.id,
-              name: label.name,
-            };
-          }
-        });
-        return request_;
-      });
-      res.json(requests_.map(_transformRequest));
-    });
   });
+  const values = await Promise.all([
+    User.findAll({
+      where: { id: requests.map(request => request.userId) },
+    }),
+    Label.findAll({
+      where: { id: requests.map(request => request.labelId) },
+    }),
+  ]);
+  const users = values[0];
+  const labels = values[1];
+
+  const requests_ = requests.map(request => {
+    const request_ = {
+      id: request.id,
+      status: request.status,
+      createdAt: request.createdAt,
+      updatedAt: request.updatedAt,
+    };
+    users.forEach(user => {
+      if (request.userId === user.id) {
+        request_.member = {
+          id: user.id,
+          name: user.name,
+          imageUrl: user.imageUrl,
+        };
+      }
+    });
+    labels.forEach(label => {
+      if (request.labelId === label.id) {
+        request_.label = {
+          id: label.id,
+          name: label.name,
+        };
+      }
+    });
+    return request_;
+  });
+  res.json(requests_.map(_transformRequest));
 }
 
-function createRequestHandler(req, res) {
+async function createRequestHandler(req, res) {
   const userId = req.user.id;
   const labelId = req.body.labelId;
   const memberId = req.body.memberId || null;
 
-  Request.findOrCreate({
+  const rawRequest = await Request.findOrCreate({
     where: {
       userId,
       labelId,
@@ -88,77 +86,68 @@ function createRequestHandler(req, res) {
       memberId,
       status: 'pending',
     },
-  }).spread((rawRequest) => {
-    const request = rawRequest.dataValues;
-    Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]).then(res_ => {
-      const label = res_[0];
-      const member = res_[1];
-      request.member = member;
-      request.label = label;
-      res.json(_transformRequest(request));
-    });
   });
+  const request = rawRequest[0].dataValues;
+  const values = await Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]);
+  const label = values[0];
+  const member = values[1];
+  request.member = member;
+  request.label = label;
+  res.json(_transformRequest(request));
 }
 
-function updateRequestHandler(req, res) {
+async function updateRequestHandler(req, res) {
   const requestId = req.params.id;
   const status = req.body.status;
 
   switch (status) {
     case 'accepted': {
-      Request.acceptOne({
+      const request = await Request.acceptOne({
         where: { id: requestId },
         individualHooks: true,
-      }).then(request => {
-        Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]).then(res_ => {
-          const label = res_[0];
-          const member = res_[1];
-          request.member = member;
-          request.label = label;
-          res.json(_transformRequest(request));
-        });
       });
+      const values = await Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]);
+      const label = values[0];
+      const member = values[1];
+      request.member = member;
+      request.label = label;
+      res.json(_transformRequest(request));
       break;
     }
     default: {
-      Request.update(
+      const request = await Request.update(
         { status },
         {
           where: { id: requestId },
           individualHooks: true,
         },
-      ).then(request => {
-        Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]).then(res_ => {
-          const label = res_[0];
-          const member = res_[1];
-          request.member = member;
-          request.label = label;
-          res.json(_transformRequest(request));
-        });
-      });
+      );
+      const values = await Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]);
+      const label = values[0];
+      const member = values[1];
+      request.member = member;
+      request.label = label;
+      res.json(_transformRequest(request));
       break;
     }
   }
 }
 
-function destroyRequestHandler(req, res) {
+async function destroyRequestHandler(req, res) {
   const requestId = req.params.id;
 
-  Request.findById(requestId).then(request => {
-    Request.destroy({
-      where: {
-        id: requestId,
-      },
-    }).then(() => {
-      Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]).then(res_ => {
-        const label = res_[0];
-        const member = res_[1];
-        request.member = member;
-        request.label = label;
-        res.json(_transformRequest(request));
-      });
-    });
+  const request = await Request.findById(requestId);
+  await Request.destroy({
+    where: {
+      id: requestId,
+    },
   });
+  const values = await Promise.all([Label.findById(request.labelId), User.findById(request.memberId)]);
+  const label = values[0];
+  const member = values[1];
+  request.member = member;
+  request.label = label;
+  res.json(_transformRequest(request));
 }
 
 module.exports = {
