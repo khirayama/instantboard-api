ee30d3918d64e112915387e0874d0cbfe0260399
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-02-11 23:18:43 +0900

    Use async/await for task handlers

--- src/handlers/task-handlers.js
+++ src/handlers/task-handlers.js
@@ -23,199 +23,167 @@ function _transformTask(task, label) {
   };
 }
 
-function _indexTask(userId) {
-  return new Promise(resolve => {
-    Label.findAllFromStatus({
-      where: { userId },
-    }).then(labels => {
-      const labelIds = labels.map(label => label.id);
-      Task.findAll({
-        where: { labelId: labelIds },
-        order: [['priority', 'ASC']],
-      }).then(tasks => {
-        const formattedTasks = tasks.map(task => {
-          for (let i = 0; i < labels.length; i++) {
-            const label = labels[i];
-            if (label.id === task.labelId) {
-              return _transformTask(task, label);
-            }
-          }
-          return null;
-        });
-        resolve(formattedTasks);
-      });
-    });
+async function _indexTask(userId) {
+  const labels = await Label.findAllFromStatus({
+    where: { userId },
   });
+  const labelIds = labels.map(label => label.id);
+  const tasks = await Task.findAll({
+    where: { labelId: labelIds },
+    order: [['priority', 'ASC']],
+  });
+  const formattedTasks = tasks.map(task => {
+    for (let i = 0; i < labels.length; i++) {
+      const label = labels[i];
+      if (label.id === task.labelId) {
+        return _transformTask(task, label);
+      }
+    }
+    return null;
+  });
+  return formattedTasks;
 }
 
-function indexTaskHandler(req, res) {
+async function indexTaskHandler(req, res) {
   const userId = req.user.id;
 
-  _indexTask(userId).then(tasks => {
-    res.json(tasks);
-  });
+  const tasks = await _indexTask(userId);
+  res.json(tasks);
 }
 
-function createTaskHandler(req, res) {
+async function createTaskHandler(req, res) {
   const userId = req.user.id;
   const labelId = req.body.labelId;
   const content = req.body.content;
 
-  Task.createWithPriority({
+  const task = await Task.createWithPriority({
     userId,
     labelId,
     content,
-  }).then(task => {
-    Label.findByIdAndUser(labelId, userId).then(label => {
-      res.json(_transformTask(task, label));
-    });
   });
+  const label = await Label.findByIdAndUser(labelId, userId);
+  res.json(_transformTask(task, label));
 }
 
-function showTaskHandler(req, res) {
+async function showTaskHandler(req, res) {
   const userId = req.user.id;
   const taskId = req.params.id;
 
-  Task.findById(taskId).then(task => {
-    Label.findByIdAndUser(task.labelId, userId).then(label => {
-      res.json(_transformTask(task, label));
-    });
-  });
+  const task = await Task.findById(taskId);
+  const label = await Label.findByIdAndUser(task.labelId, userId);
+  res.json(_transformTask(task, label));
 }
 
-function updateTaskHandler(req, res) {
+async function updateTaskHandler(req, res) {
   const userId = req.user.id;
   const taskId = req.params.id;
   const labelId = req.body.labelId;
   const content = req.body.content;
   const completed = req.body.completed;
 
-  Task.findById(taskId).then(task => {
-    if (labelId && String(labelId) !== String(task.labelId)) {
-      Promise.all([
-        Task.count({
-          where: { labelId },
-        }),
-        Task.findAll({
-          where: {
-            labelId: task.labelId,
-            priority: {
-              [Sequelize.Op.gt]: task.priority,
-            },
-          },
-        }),
-      ]).then(values => {
-        const count = values[0];
-        const tasks = values[1];
-
-        tasks.forEach(task_ => {
-          task_.update({ priority: task_.priority - 1 });
-        });
-
-        task
-          .update({
-            labelId: labelId === undefined ? task.labelId : labelId,
-            content: content === undefined ? task.content : content,
-            completed: completed === undefined ? task.completed : completed,
-            priority: count,
-          })
-          .then(task_ => {
-            Label.findByIdAndUser(task_.labelId, userId).then(label => {
-              res.json(_transformTask(task_, label));
-            });
-          });
-      });
-    } else {
-      task
-        .update({
-          content: content === undefined ? task.content : content,
-          completed: completed === undefined ? task.completed : completed,
-        })
-        .then(task_ => {
-          Label.findByIdAndUser(task_.labelId, userId).then(label => {
-            res.json(_transformTask(task_, label));
-          });
-        });
-    }
-  });
-}
-
-function destroyTaskHandler(req, res) {
-  const userId = req.user.id;
-  const taskId = req.params.id;
-
-  Task.findById(taskId).then(task => {
-    Task.findAll({
-      where: {
-        labelId: task.labelId,
-        priority: {
-          [Sequelize.Op.gt]: task.priority,
-        },
-      },
-    }).then(tasks => {
-      tasks.forEach(task_ => {
-        task_.update({ priority: task_.priority - 1 });
-      });
-    });
-
-    task.destroy().then(destroyedTask => {
-      Label.findByIdAndUser(destroyedTask.labelId, userId).then(label => {
-        res.json(_transformTask(task, label));
-      });
-    });
-  });
-}
-
-function sortTaskHandler(req, res) {
-  const userId = req.user.id;
-  const taskId = req.params.id;
-  const priority = req.body.priority;
-
-  Task.findById(taskId).then(task => {
-    if (task.priority < priority) {
+  const task = await Task.findById(taskId);
+  if (labelId && String(labelId) !== String(task.labelId)) {
+    const values = await Promise.all([
+      Task.count({
+        where: { labelId },
+      }),
       Task.findAll({
         where: {
           labelId: task.labelId,
           priority: {
             [Sequelize.Op.gt]: task.priority,
-            [Sequelize.Op.lte]: priority,
           },
         },
-      }).then(tasks => {
-        tasks.forEach(task_ => {
-          task_.update({ priority: task_.priority - 1 });
-        });
-        task.update({ priority }).then(() => {
-          _indexTask(userId).then(tasks => {
-            res.json(tasks);
-          });
-        });
-      });
-    } else if (task.priority > priority) {
-      Task.findAll({
-        where: {
-          labelId: task.labelId,
-          priority: {
-            [Sequelize.Op.gte]: priority,
-            [Sequelize.Op.lt]: task.priority,
-          },
-        },
-      }).then(tasks => {
-        tasks.forEach(task_ => {
-          task_.update({ priority: task_.priority + 1 });
-        });
-        task.update({ priority }).then(() => {
-          _indexTask(userId).then(tasks => {
-            res.json(tasks);
-          });
-        });
-      });
-    } else {
-      _indexTask(userId).then(tasks => {
-        res.json(tasks);
-      });
-    }
+      }),
+    ]);
+    const count = values[0];
+    const tasks = values[1];
+
+    tasks.forEach(task_ => {
+      task_.update({ priority: task_.priority - 1 });
+    });
+    const task_ = await task.update({
+      labelId: labelId === undefined ? task.labelId : labelId,
+      content: content === undefined ? task.content : content,
+      completed: completed === undefined ? task.completed : completed,
+      priority: count,
+    });
+    const label = await Label.findByIdAndUser(task_.labelId, userId);
+    res.json(_transformTask(task_, label));
+  } else {
+    const task_ = await task.update({
+      content: content === undefined ? task.content : content,
+      completed: completed === undefined ? task.completed : completed,
+    });
+    const label = await Label.findByIdAndUser(task_.labelId, userId);
+    res.json(_transformTask(task_, label));
+  }
+}
+
+async function destroyTaskHandler(req, res) {
+  const userId = req.user.id;
+  const taskId = req.params.id;
+
+  const task = await Task.findById(taskId);
+  const tasks = await Task.findAll({
+    where: {
+      labelId: task.labelId,
+      priority: {
+        [Sequelize.Op.gt]: task.priority,
+      },
+    },
   });
+  tasks.forEach(task_ => {
+    task_.update({ priority: task_.priority - 1 });
+  });
+
+  const destroyedTask = await task.destroy();
+  const label = await Label.findByIdAndUser(destroyedTask.labelId, userId);
+  res.json(_transformTask(task, label));
+}
+
+async function sortTaskHandler(req, res) {
+  const userId = req.user.id;
+  const taskId = req.params.id;
+  const priority = req.body.priority;
+
+  const task = await Task.findById(taskId);
+  if (task.priority < priority) {
+    const tasks = await Task.findAll({
+      where: {
+        labelId: task.labelId,
+        priority: {
+          [Sequelize.Op.gt]: task.priority,
+          [Sequelize.Op.lte]: priority,
+        },
+      },
+    });
+    tasks.forEach(task_ => {
+      task_.update({ priority: task_.priority - 1 });
+    });
+    await task.update({ priority });
+    const tasks_ = _indexTask(userId);
+    res.json(tasks_);
+  } else if (task.priority > priority) {
+    const tasks = await Task.findAll({
+      where: {
+        labelId: task.labelId,
+        priority: {
+          [Sequelize.Op.gte]: priority,
+          [Sequelize.Op.lt]: task.priority,
+        },
+      },
+    });
+    tasks.forEach(task_ => {
+      task_.update({ priority: task_.priority + 1 });
+    });
+    await task.update({ priority });
+    const tasks_ = await _indexTask(userId);
+    res.json(tasks_);
+  } else {
+    const tasks = await _indexTask(userId);
+    res.json(tasks);
+  }
 }
 
 module.exports = {
